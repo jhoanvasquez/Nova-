b065ede86c62f55f1894924eb04333f32961fb2b
jhoanVasquez <jhoan.vasquez@correounivalle.edu.co>
2020-04-27 01:42:58 -0500

    subida

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/reserva/busqueda', "ReservaController@busqueda");
+Route::post('/user/busquedaUsuario', "UserController@busquedaUsuario");
+Route::post('/reserva/busquedaReserva', "ReservaController@busquedaReserva");
+Route::get('/reserva/today', "ReservaController@today");
+
+
+
+Route::Resource('reserva', "ReservaController");
+Route::Resource('user', "UserController");
+Route::Resource('saldo', "SaldoController");
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
