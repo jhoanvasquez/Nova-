9cdfe7caefa20bbca8a86a9654f6d1ec9ecbbd7b
jhoanVasquez <jhoan.vasquez@correounivalle.edu.co>
2020-04-27 01:30:56 -0500

    subida

---
+++ database/migrations/2020_03_31_182036_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->string('id');
+            $table->string("name");
+            $table->string("apellido");
+            $table->string("email");
+            $table->string("password");
+            $table->string("tipo_usuario");
+            $table->integer('saldo');
+            $table->string("avatar");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
---
+++ database/migrations/2020_03_31_182051_create_inventarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventarios', function (Blueprint $table) {
+            $table->integer('canchas_reservadas');
+            $table->integer('ganancias');
+            $table->integer('saldo')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventarios');
+    }
+}
